ac28e891c4131b79218f19cf74cdd6cfdf668f93
sahilkumardev <sahilkumar710@outlook.com>
2025-05-29 14:12:21 +0530

    Add theme provider and toggle components for dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import ThemeToggle from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div className="text-4xl h-screen flex flex-col justify-center items-center font-mono font-medium">
-      Sahil Kumar Dev <Button className="mt-2">Click to connect.</Button>
+      Sahil Kumar Dev
+      <span className="mt-4 flex flex-col items-center gap-2">
+        <Button>Click to connect.</Button>
+        <ThemeToggle />
+      </span>
     </div>
   );
 };
