53bc04b822540441ac994c51cd06ffee17af6d7c
sahilkumardev <sahilkumar710@outlook.com>
2025-05-29 14:00:48 +0530

    Add new components and update styles for improved UI consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div className="text-4xl h-screen flex justify-center items-center font-mono font-medium">
-      Sahil Kumar Dev
+    <div className="text-4xl h-screen flex flex-col justify-center items-center font-mono font-medium">
+      Sahil Kumar Dev <Button className="mt-2">Click to connect.</Button>
     </div>
   );
 };
