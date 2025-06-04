ea461dce6452de9144726a5aa93eb09d605545ce
sahilkumardev <sahilkumar710@outlook.com>
2025-06-05 00:03:44 +0530

    Implement Contact page with input fields and submission button; add Input and Textarea components

--- app/(app)/contact/page.tsx
+++ app/(app)/contact/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="mt-32 px-6">
+      <h1>Design Inquiry</h1>
+      <p>Got an idea and need design help? Reach out now</p>
 
-export default Contact
+      <div>
+        <div>
+          <Input />
+          <Input />
+        </div>
+        <Textarea />
+
+        <Button>Submit</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
